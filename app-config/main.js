a77c59aea368994b203454fa927803a0371e2958
Fiore Basile <fiore.basile@gmail.com>
2016-06-12 15:08:14 +0200

    missing final projects

--- app-config/main.js
+++ app-config/main.js
@@ -1,204 +1,198 @@
 A.app({
-        appName: 'Fab Academy - Final Project Management',
-        appIcon: 'globe',
-        allowSignUp: false,
-        onlyAuthenticated: true,
-        roles: ['instruct', 'eval'],
-        menuItems: [{
-            name: 'Students',
-            icon: 'users',
-            entityTypeId: 'Student',
+    appName: 'Fab Academy - Final Project Management',
+    appIcon: 'globe',
+    allowSignUp: false,
+    onlyAuthenticated: true,
+    roles: ['instruct', 'eval'],
+    menuItems: [{
+        name: 'Students',
+        icon: 'users',
+        entityTypeId: 'Student',
+    }, {
+        name: 'Labs',
+        icon: 'book',
+        entityTypeId: 'Lab'
+    }, {
+        name: 'Final Projects',
+        icon: 'final',
+        children: [{
+            name: 'Sessions',
+            icon: 'table',
+            entityTypeId: 'Session'
         }, {
-            name: 'Labs',
-            icon: 'book',
-            entityTypeId: 'Lab'
+            name: 'Schedule',
+            icon: 'table',
+            entityTypeId: 'FinalProjectSchedule'
         }, {
-            name: 'Final Projects',
-            icon: 'final',
-            children: [{
-                name: 'Sessions',
-                icon: 'table',
-                entityTypeId: 'Session'
-            }, {
-                name: 'Schedule',
-                icon: 'table',
-                entityTypeId: 'FinalProjectSchedule'
-            }, {
-                name: 'Presented',
-                icon: 'table',
-                entityTypeId: 'FinalProjectPresented'
-            }, {
-                name: 'Missing',
-                icon: 'table',
-                entityTypeId: 'MissingFinalProject'
-            }]
-        }],
-        entities: function(Fields) {
-            return {
-                User: {
+            name: 'Presented',
+            icon: 'table',
+            entityTypeId: 'FinalProjectPresented'
+        }, {
+            name: 'Missing',
+            icon: 'table',
+            entityTypeId: 'MissingFinalProject'
+        }]
+    }],
+    entities: function(Fields) {
+        return {
+            User: {
+                
+            },
+            Student: {
+                title: 'Students',
+                referenceName: 'fullname',
+                permissions: {
+                    read: null,
+                    write: ['instruct','eval'],
+                    delete: []  
+                },
+                fields: {
+                    student_id: Fields.text('Student ID').required(),
+                    fullname: Fields.text('Full name').readOnly(),
+                    name: Fields.text('Name').required(),
+                    surname: Fields.text('Surname').required(),
+                    email: Fields.email('Email').required(),
+                    website: Fields.link('Website'),
+                    notes: Fields.textarea('Notes'),
+                    graduated: Fields.checkbox('Graduated').readOnly(),
+                    completed: Fields.checkbox('Completed').readOnly(),
+                    needs_eval: Fields.checkbox('Needs Re-evaluation').readOnly(),
+                    lab: Fields.fixedReference('Lab', 'Lab'),
+                    final_project_session: Fields.fixedReference("Project presentation", "Session"),
+                    final_project_presented: Fields.checkbox('Project presented'),
+                    final_project_slide: Fields.link('Project Slide URL'),
+                    final_project_video: Fields.link('Project Video URL')
 
                 },
-                Student: {
-                    title: 'Students',
-                    referenceName: 'fullname',
-                    permissions: {
-                        read: null,
-                        write: ['instruct', 'eval'],
-                        delete: []
-                    },
-                    fields: {
-                        student_id: Fields.text('Student ID').required(),
-                        fullname: Fields.text('Full name').readOnly(),
-                        name: Fields.text('Name').required(),
-                        surname: Fields.text('Surname').required(),
-                        email: Fields.email('Email').required(),
-                        website: Fields.link('Website'),
-                        notes: Fields.textarea('Notes'),
-                        graduated: Fields.checkbox('Graduated').readOnly(),
-                        completed: Fields.checkbox('Completed').readOnly(),
-                        needs_eval: Fields.checkbox('Needs Re-evaluation').readOnly(),
-                        lab: Fields.fixedReference('Lab', 'Lab'),
-                        final_project_session: Fields.fixedReference("Project presentation", "Session"),
-                        final_project_presented: Fields.checkbox('Project presented'),
-                        final_project_slide: Fields.link('Project Slide URL'),
-                        final_project_video: Fields.link('Project Video URL')
+                actions: [{
+                    id: 'reload',
+                    name: 'Reload',
+                    actionTarget: 'single-item',
+                    perform: function(Crud, Actions, Console, Q, Pipes) {
+                        var crud = Crud.actionContextCrud();
+                        return crud.readEntity(Actions.selectedEntityId()).then(function(provider) {
+                            return Actions.refreshResult();
+                        });
+                    }
+                }, {
+                    id: 'graduate',
+                    name: 'Graduate',
+                    actionTarget: 'single-item',
+                    perform: function(Crud, Actions) {
+                        var crud = Crud.actionContextCrud();
+                        return crud.readEntity(Actions.selectedEntityId()).then(function(entity) {
+                            entity.graduated = true;
+                            entity.completed = true;
+                            entity.needs_eval = false;
+                            return crud.updateEntity(entity);
+                        }).then(function() {
+                            return Actions.refreshResult();
+                        });
+                    }
+                },  {
+                    id: 'complete',
+                    name: 'Mark complete',
+                    actionTarget: 'single-item',
+                    perform: function(Crud, Actions) {
+                        var crud = Crud.actionContextCrud();
+                        return crud.readEntity(Actions.selectedEntityId()).then(function(entity) {
+                            entity.completed = true;
+                            entity.graduated = false;
+                            entity.needs_eval = false;
+                            return crud.updateEntity(entity);
+                        }).then(function() {
+                            return Actions.refreshResult();
+                        });
+                    }
+                },  {
+                    id: 'needseval',
+                    name: 'Need Re-Eval',
+                    actionTarget: 'single-item',
+                    perform: function(Crud, Actions) {
+                        var crud = Crud.actionContextCrud();
+                        return crud.readEntity(Actions.selectedEntityId()).then(function(entity) {
+                            entity.graduated = false;
+                            entity.completed = false;
+                            entity.needs_eval = true;
+                            return crud.updateEntity(entity);
+                        }).then(function() {
+                            return Actions.refreshResult();
+                        });
+                    }
+                }],
+                showInGrid: ['student_id', 'name', 'surname', 'email', 'lab'],
+                sorting: [
+                    ['student_id', 1],
+                    ['surname', 1],
+                    ['name', 1]
+                ],
+                beforeSave: function(Entity, Crud, ValidationError) {
+                    Entity.fullname = Entity.name + ' ' + Entity.surname;
+                    var crud = Crud.crudFor('Student');
+                    var crud2 = Crud.crudFor('Session');
 
-                    },
-                    actions: [{
-                        id: 'reload',
-                        name: 'Reload',
-                        actionTarget: 'single-item',
-                        perform: function(Crud, Actions, Console, Q, Pipes) {
-                            var crud = Crud.actionContextCrud();
-                            return crud.readEntity(Actions.selectedEntityId()).then(function(provider) {
-                                return Actions.refreshResult();
-                            });
-                        }
-                    }, {
-                        id: 'graduate',
-                        name: 'Graduate',
-                        actionTarget: 'single-item',
-                        perform: function(Crud, Actions) {
-                            var crud = Crud.actionContextCrud();
-                            return crud.readEntity(Actions.selectedEntityId()).then(function(entity) {
-                                entity.graduated = true;
-                                entity.completed = true;
-                                entity.needs_eval = false;
-                                return crud.updateEntity(entity);
-                            }).then(function() {
-                                return Actions.refreshResult();
-                            });
-                        }
-                    }, {
-                        id: 'complete',
-                        name: 'Mark complete',
-                        actionTarget: 'single-item',
-                        perform: function(Crud, Actions) {
-                            var crud = Crud.actionContextCrud();
-                            return crud.readEntity(Actions.selectedEntityId()).then(function(entity) {
-                                entity.completed = true;
-                                entity.graduated = false;
-                                entity.needs_eval = false;
-                                return crud.updateEntity(entity);
-                            }).then(function() {
-                                return Actions.refreshResult();
-                            });
-                        }
-                    }, {
-                        id: 'needseval',
-                        name: 'Need Re-Eval',
-                        actionTarget: 'single-item',
-                        perform: function(Crud, Actions) {
-                            var crud = Crud.actionContextCrud();
-                            return crud.readEntity(Actions.selectedEntityId()).then(function(entity) {
-                                entity.graduated = false;
-                                entity.completed = false;
-                                entity.needs_eval = true;
-                                return crud.updateEntity(entity);
-                            }).then(function() {
-                                return Actions.refreshResult();
-                            });
-                        }
-                    }],
-                    showInGrid: ['student_id', 'name', 'surname', 'email', 'lab'],
-                    sorting: [
-                        ['student_id', 1],
-                        ['surname', 1],
-                        ['name', 1]
-                    ],
-                    beforeSave: function(Entity, Crud, ValidationError) {
-                        Entity.fullname = Entity.name + ' ' + Entity.surname;
-                        var crud = Crud.crudFor('Student');
-                        var crud2 = Crud.crudFor('Session');
+                    if (Entity.final_project_session) {
 
-                        if (Entity.final_project_session) {
+                        return crud2.readEntity(Entity.final_project_session.id).then(function(session) {
 
-                            return crud2.readEntity(Entity.final_project_session.id).then(function(session) {
+                            var filteringDict = {
+                                final_project_session: session.id
+                            };
 
-                                var filteringDict = {
-                                    final_project_session: session.id
-                                };
-
-                                return crud.find({
-                                    filtering: filteringDict
-                                }).then(function(items) {
-                                    var incr = 1;
-                                    for (var i = 0; i < items.length; i++) {
-                                        if (items[i].id == Entity.id) {
-                                            incr = 0;
-                                        }
+                            return crud.find({
+                                filtering: filteringDict
+                            }).then(function(items) {
+                                var incr = 1;
+                                for (var i = 0; i < items.length; i++) {
+                                    if (items[i].id == Entity.id) {
+                                        incr = 0;
                                     }
-                                    if (items.length + incr <= session.max_students) {
+                                }
+                                if (items.length + incr <= session.max_students) {
 
-                                        return crud2.updateEntity({
-                                            id: session.id,
-                                            students_count: items.length + incr
-                                        });
-                                    } else {
-                                        throw new ValidationError({
-                                            final_project_session: 'The maximum number of students for this session has been reached.'
-                                        });
-                                    }
-
-                                });
+                                    return crud2.updateEntity({
+                                        id: session.id,
+                                        students_count: items.length + incr
+                                    });
+                                } else {
+                                    throw new ValidationError({
+                                        final_project_session: 'The maximum number of students for this session has been reached.'
+                                    });
+                                }
 
                             });
-                        }
-                    },
-                    afterSave: function(Entity, Crud, Q) {
-                        return updateSessionCounts(Crud, Q);
-                    },
-                    views: {
-                        FinalProjectSchedule: {
-                            title: 'Final Project Schedule',
-                            filtering: 'final_project_session != null',
-                            showInGrid: ['final_project_session', 'lab', 'student_id', 'name', 'surname', 'final_project_presented'],
-                            sorting: [
-                                ['final_project_session', 1],
-                                ['lab', 1]
-                            ]
-                        },
-                        FinalProjectPresented: {
-                            title: 'Final Project Presented',
-                            filtering: 'final_project_presented = true',
-                        },
-                        MissingFinalProject: {
-                            title: 'Missing Final Project',
-                            filtering: {
-                                $or: [
-                                    "final_project_session": {
-                                        "$type": 10
-                                    }
-                                },
-                                {
-                                    "final_project_session": {
-                                        "$exists": false
-                                    }
-                                }]
-                        },
+
+                        });
+                    }
+                },
+                afterSave: function(Entity, Crud,Q) {
+                    return updateSessionCounts(Crud,Q);
+                },
+                views: {
+                    FinalProjectSchedule: {
+                        title: 'Final Project Schedule',
+                        filtering: 'final_project_session != null',
+                        showInGrid: ['final_project_session', 'lab', 'student_id', 'name', 'surname', 'final_project_presented'],
                         sorting: [
-                            ['lab.name', 1]
+                            ['final_project_session', 1],
+                            ['lab', 1]
                         ]
+                    },
+                    FinalProjectPresented: {
+                        title: 'Final Project Presented',
+                        filtering: 'final_project_presented = true',
+                    },
+                    MissingFinalProject: {
+                        title: 'Missing Final Project',
+                        filtering: {
+                            "final_project_session" : {
+                                "$type": 10
+                            },
+                            "final_project_session" : {
+                                "$exists": false
+                            }
+                        },
+                        sorting: [["surname", 1], ["name", 1]]
                     },
                     StudentsForLab: {
                         title: 'Enrolled Students',
@@ -219,8 +213,8 @@ A.app({
                 referenceName: 'session',
                 permissions: {
                     read: null,
-                    write: ['instruct', 'eval'],
-                    delete: ['eval']
+                    write: ['instruct','eval'],
+                    delete: ['eval']  
                 },
                 fields: {
                     name: Fields.text('Name'),
@@ -254,8 +248,8 @@ A.app({
                 referenceName: 'name',
                 permissions: {
                     read: null,
-                    write: ['instruct', 'eval'],
-                    delete: []
+                    write: ['instruct','eval'],
+                    delete: []  
                 },
                 fields: {
                     lab_id: Fields.text('Lab ID'),
@@ -274,13 +268,13 @@ A.app({
     }
 });
 
-function updateSessionCounts(Crud, Q) {
+function updateSessionCounts(Crud,Q) {
     var crud = Crud.crudFor('Session');
 
     return crud.find({}).then(function(items) {
         return Q.all(
             items.map(function(session) {
-
+           
                 return Crud.crudFor('Student').find({
                     filtering: {
                         final_project_session: session.id
